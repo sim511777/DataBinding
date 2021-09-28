5e01aa0f088823b232935ded34361b32170e3a84
sim511777 <sim51177@gmail.com>
2021-09-29 06:54:13 +0900

    데이터 바인딩 할수 있는 사용자 정의 오브젝트 생성

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -4,16 +4,48 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace WindowsFormsApp1 {
-   public partial class Form1 : Form {
-      public Form1() {
-         InitializeComponent();
-         var bindingSource = new BindingSource(this.textBox1, null);
-         this.DataBindings.Add("Text", bindingSource, "Text");
-      }
-   }
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+            textBox1.DataBindings.Add("Text", textBox2, "Text", false, DataSourceUpdateMode.OnPropertyChanged);
+
+            // 데이터 바인딩 추가
+            textBox4.DataBindings.Add("Text", source, "Text", false, DataSourceUpdateMode.OnPropertyChanged);
+            //source.PropertyChanged += (s, e) => { textBox4.Text = source.Text; };
+            //textBox4.TextChanged += (s, e) => { source.Text = textBox4.Text; };
+        }
+
+        Source source = new Source();
+
+        // 소스 변경
+        private void textBox3_TextChanged(object sender, EventArgs e) {
+            source.Text = textBox3.Text;
+        }
+
+        // 소스 확인
+        private void button1_Click(object sender, EventArgs e) {
+            this.Text = source.Text;
+        }
+    }
+
+    class Source : INotifyPropertyChanged {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string text;
+        // set 속성에서 이벤트 호출
+        public string Text {
+            get { return text; }
+            set { text = value; NotifyPropertyChanged(); }
+        }
+
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "") {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
 }
